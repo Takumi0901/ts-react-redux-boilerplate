bb3be257f4a8a309c6255fbb26f980e0ef0b129e
takumi kimura <juventus0901@gmail.com>
2018-10-23 18:15:54 +0900

    Fix containers

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import App from 'src/containers/00_app/App'
-import Home from 'src/containers/01_home/pages/Home'
-import Users from 'src/containers/02_users/pages/Users'
+import App from 'src/containers/app/App'
+import Home from 'src/containers/home/pages/Home'
+import Users from 'src/containers/users/pages/Users'
 
 export default (
   <React.Fragment>
